9b0adbd2ef899f7524d8117a1d1e62242560b40c
Calvin <calvinhlee23@gmail.com>
2016-11-25 13:27:55 -0800

    basic blocks work. including 123movies and youtube

--- ad_urls.js
+++
@@ -1,4 +0,0 @@
-
-const AD_URLS = ["*://www.evil.com*"];
-
-export default AD_URLS;
--- block.js
+++ block.js
@@ -1,14 +1,15 @@
-
 var bkg = chrome.extension.getBackgroundPage();
 bkg.console.log('foo');
 
 const blockFunction = (details) => {
-  return bkg.console.log(details);
+  return { cancel: true};
 };
-const options = ["blocking"];
-document.addEventListener("beforeLoad", () => {
 
-});
+const options = ["blocking"];
 
 var webRequest = chrome.webRequest;
-webRequest.onBeforeRequest.addListener( blockFunction,{urls: ["<all_urls>"]});
+const AD_URLS = ["*://scontent-yyz1-1.xx.fbcdn.net/*", "*://onclickads.net/*",
+      "*://*/banner/*", "*://go.moonclick/*",
+      "*://creative.wwwpromoter.com/*","*://*.googlevideo.com/crossdomain.xml",
+      "*://*/ads/*"];
+webRequest.onBeforeRequest.addListener( blockFunction,{urls: AD_URLS}, options);
